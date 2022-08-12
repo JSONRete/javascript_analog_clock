3d44586f4c574a03469118f4cef6b7d2701c2edb
JSONRete <jsonrete@gmail.com>
2022-08-12 10:31:10 -0400

    creating and testing the getTime()

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const secondsHand = document.getElementById('seconds-hand')
 const minutesHand = document.getElementById('seconds-hand')
 const hoursHand = document.getElementById('seconds-hand')
+
+function getTime() {
+    const now = new Date()
+    const seconds = now.getSeconds()
+    const minutes = now.getMinutes()
+    const hours = now.getHours()
+
+    console.log(now)
+}
+getTime()
